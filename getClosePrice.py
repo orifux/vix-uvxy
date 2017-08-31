ce9f6380b0ce547221e03ec9bc62584d88cac407
ori <ori@ori-K54C>
2017-09-01 00:42:58 +0300

    getPrice

--- getClosePrice.py
+++ getClosePrice.py
@@ -1,14 +1,20 @@
 import pandas
 import requests
 import io
+import datetime
+from datetime import timedelta  
 
-def getClosePrice(stock, date, source):
+#http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/vixcurrent.csv
+
+
+
+def getUVXYClosePrice(stock, date, source):
     stock = stock
     startdate = date
     enddate = date
 
     rooturl = 'http://www.google.com/finance/historical?q='
-    query = stock + '&startdate=' + startdate +'&enddate=' + enddate #+ '&output=csv'
+    query = stock + '&startdate=' + startdate +'&enddate=' + enddate + '&output=csv'
     
     url = rooturl + query
     print url
@@ -20,25 +26,19 @@ def getClosePrice(stock, date, source):
         return df.get_value(0,'Close');
     except:
         return -1;    
-
-def getClosePrice(stock, date):
-    stock = stock
-    startdate = date
-    enddate = date
-
-    rooturl = 'http://www.google.com/finance/historical?q='
-    query = stock + '&startdate=' + startdate +'&enddate=' + enddate #+ '&output=csv'
     
-    url = rooturl + query
-    print url
+def getVIXClosePrice(date):
+    
+    url = 'http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/vixcurrent.csv'
     response = requests.get(url)
 
-    df = pandas.read_csv(io.StringIO(response.content.decode('utf-8')))
-    print(df)
+    df = pandas.read_csv(io.StringIO(response.content.decode('utf-8')),skiprows=1,index_col=0)
     try:
-        return df.get_value(0,'Close');
+        return df.loc[date,'VIX Close']
     except:
-        return -1;    
+        return -1;   
 
-https://ichart.yahoo.com/table.csv?s=AAPL&a=0&b=1&c=2010&d=11&e=31&f=2015&g=m&ignore=.csv
-print getClosePrice('UVXY', 'Jul 27, 2017')
+#print getVixClosePrice('08/22/2017')
+currDate = (datetime.date.today() - datetime.timedelta(days=2)).strftime("%m/%d/%Y")
+print currDate
+print getVIXClosePrice(currDate)
