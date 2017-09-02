80df9f9eea39fe2cc4550fe0f990c6dbef095369
ori <ori@ori-K54C>
2017-09-02 22:29:41 +0300

    # WARNING: head commit changed in the meantime
    ignore

--- getClosePrice.py
+++ getClosePrice.py
@@ -1,4 +1,4 @@
-import sys
+import numpy as np
 import pandas
 import requests
 import io
@@ -33,14 +33,16 @@ def getStockClosePrice(stock, date):
     except:
         return -1;    
     
-def getVIXClosePrice(date):
+def getVIXClosePrice(startDate,endDate):
     
     url = 'http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/vixcurrent.csv'
     response = requests.get(url)
 
     df = pandas.read_csv(io.StringIO(response.content.decode('utf-8')),skiprows=1,index_col=0)
+    #print df
     try:
-        return df.loc[date.strftime("%m/%d/%Y"),'VIX Close']
+        df=df.loc[startDate.strftime("%m/%d/%Y"):endDate.strftime("%m/%d/%Y"),'VIX Close']
+        return df
     except:
         return -1;   
 
@@ -48,25 +50,56 @@ def getVIXClosePrice(date):
 def main():
     #print getVixClosePrice('08/22/2017')
     #currDate = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%m/%d/%Y")
-    currDate = (datetime.date.today() - datetime.timedelta(days=1))
+    startDate = (datetime.date.today() - datetime.timedelta(days=4))
+    endDate = (datetime.date.today() - datetime.timedelta(days=1))
     #print currDate
     #print currDate.strftime("%m/%d/%Y")
-    print getVIXClosePrice(currDate)
+    df_vixClosePrice=getVIXClosePrice(startDate,endDate)
+    print df_vixClosePrice.index.values
+    #print getStockClosePrice('UVXY', startDate)
     
-    print getStockClosePrice('UVXY', currDate)
+        
+    todays_date = datetime.datetime.now().date()
+    index = pandas.date_range(todays_date-datetime.timedelta(4), periods=4, freq='D')
+    #print index[1]
+    
+    #columns = ['pos_num','open_pos_date','vix_pos_price','uvxy_pos_price','uvxy_vix_pos_ratio','uvxy_vix_curr_pos','os_vix_qty','pos_vix_op_price','pos_vix_strick','pos_vix_exp','pos_uvxy_qty','pos_uvxy_op_price','pos_uvxy_strick','pos_uvxy_exp','pos_vix_amount','pos_uvxy_amount','curr_pos_vix_value','curr_pos_uvxy_value','days_pass','is_55_days_pass','pos_ratio*0.75 >=curr_ratio','pos_status','pos_gain']
+    columns = ['pos_num','open_pos_date']##,'vix_pos_price','uvxy_pos_price','uvxy_vix_pos_ratio','uvxy_vix_curr_pos','os_vix_qty','pos_vix_op_price','pos_vix_strick','pos_vix_exp','pos_uvxy_qty','pos_uvxy_op_price','pos_uvxy_strick','pos_uvxy_exp','pos_vix_amount','pos_uvxy_amount','curr_pos_vix_value','curr_pos_uvxy_value','days_pass','is_55_days_pass','pos_ratio*0.75 >=curr_ratio','pos_status','pos_gain']
+    #A = np.array([[1,2],[4,5]])
+    Alist = []
+    Alist = df_vixClosePrice.values
+    print Alist
+    for i in range(100):
+        print i
+      #  newrow = np.array([i,getVIXClosePrice(index[i])])
+       # print newrow
+       # Alist.append(newrow)
+    A = np.array(Alist)
+    print A
+    print '-------'
+    df_ = pandas.DataFrame(A,index=index, columns=columns)
+    #df_ = df_.fillna(0) # with 0s rather than NaNs
+    print df_
     
 main()
 aapl = Options('uvxy', 'yahoo')
 data = aapl.get_all_data()
 ##print data
+print data.loc[(46, '2018-03-16','call'),('Last','Underlying_Price')].head()
+print data.loc[(46, '2018-03-16','call'),'Vol'].head()
 print '------------------'
 print data.iloc[0:5, 0:5]
 print '-------------'
 #Show the $100 strike puts at all expiry dates:
 print data.loc[(46, slice(None), 'call'),:] #.iloc[0:5, 0:5]
 print '-------------'
-#Show the volume traded of $100 strike puts at all expiry dates:
-print data.loc[(46, slice(None), 'put'),'Vol'].head()
+
+
+
+
+
+
+
 
 
 
